f7017777b04ab24da19224c0ce46d5c4503eb311
kromero <kromero88@gmail.com>
2019-06-06 21:40:48 -0400

    began newproj
    created simple drop down responsive nav and added some scrolling effects to it.

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,19 @@
 import '../styles/index.scss';
 
 console.log('webpack starterkit');
+
+let elm = document.querySelector('.menu-icon');
+let navMenu = document.querySelector('nav ul');
+elm.onclick = function() {
+    navMenu.classList.toggle('showing');
+};
+
+// const nav = document.querySelector('nav');
+
+window.addEventListener('scroll', function() { 
+    let nav = document.querySelector('nav');
+    let scrollpos = window.scrollY;
+    if (scrollpos > 30 ) { nav.classList.toggle('black'); }
+    else { nav.classList.remove('black'); }
+    console.log(scrollpos);
+  });
