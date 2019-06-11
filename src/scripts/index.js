d907cc1ec40c63ba8ca51d32514aaf3643b38054
kromero <kromero88@gmail.com>
2019-06-10 23:38:51 -0400

    fixed js animation issue

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -8,12 +8,13 @@ elm.onclick = function() {
     navMenu.classList.toggle('showing');
 };
 
-// const nav = document.querySelector('nav');
+const nav = document.querySelector('nav');
 
 window.addEventListener('scroll', function() { 
     let nav = document.querySelector('nav');
     let scrollpos = window.scrollY;
-    if (scrollpos > 30 ) { nav.classList.toggle('black'); }
-    else { nav.classList.remove('black'); }
+    if (scrollpos > 30 ) { nav.classList.add('black'); }
+    else if (scrollpos == 0){ nav.classList.remove('black'); }
+    else {}
     console.log(scrollpos);
   });
