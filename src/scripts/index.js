59d51fd5ba10072e7bddb3d2167478e730bd83e3
kromero <kromero88@gmail.com>
2019-06-15 06:53:48 -0400

    sx

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -13,8 +13,9 @@ const nav = document.querySelector('nav');
 window.addEventListener('scroll', function() { 
     let nav = document.querySelector('nav');
     let scrollpos = window.scrollY;
-    if (scrollpos > 30 ) { nav.classList.add('black'); }
-    else if (scrollpos == 0){ nav.classList.remove('black'); }
+    if (scrollpos >= 30) { nav.classList.add('black'); }
+    else if (scrollpos >= 689){ nav.classList.add('red'); }
+    else if (scrollpos > 1000){ nav.classList.add('black'); }
     else {}
     console.log(scrollpos);
   });
